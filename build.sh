e93fb91af8feb9f7ef5282186b50837bce6592e9
firsovvs <7513325Qq>
2022-02-21 11:11:31 +0300

    пробую докер

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 git pull
 #docker-compose -f --env-file .env.dev.prod up -d --build
-docker-compose -f --env-file .env.dev up -d --build
+#docker-compose -f --env-file .env.dev up -d --build
 
 #echo "Building server"
-#docker-compose -f ./server/docker-compose.yml up -d --build
+docker-compose -f server/docker-compose.yml up -d --build
 
 #echo "Building client"
-#docker-compose -f ./client/docker-compose.yml up -d --build
+docker-compose -f client/docker-compose.yml up -d --build
