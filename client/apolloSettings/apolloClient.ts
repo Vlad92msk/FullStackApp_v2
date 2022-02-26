689f579c06487a8c2d78808dbb840389d07ea9a0
firsovvs <7513325Qq>
2022-02-27 02:48:50 +0300

    пробую докер...

--- client/apolloSettings/apolloClient.ts
+++ client/apolloSettings/apolloClient.ts
@@ -14,7 +14,7 @@ let apolloClient: ApolloClient<NormalizedCacheObject>
 const createApolloClient = (lang: string) => new ApolloClient({
     ssrMode: typeof window === 'undefined',
     link: new HttpLink({
-      uri: `http://localhost:4000/graphql`,
+      uri: `http://localhost:3000/graphql`,
       // credentials: 'same-origin',
       headers: {
         userLanguage: lang
